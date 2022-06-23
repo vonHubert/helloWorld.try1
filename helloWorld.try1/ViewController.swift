eac7a732336ec468ee2fed956190bbbc40f6cd8a
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-06-23 09:58:39 +0300

    Initial Commit

---
+++ helloWorld.try1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloWorld.try1
+//
+//  Created by MacBook Air 13 on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
